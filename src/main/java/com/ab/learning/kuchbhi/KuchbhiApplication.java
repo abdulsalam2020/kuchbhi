4f1dd716b6e9151d5ea3e030753bb885c75c1d03
abdulsalam2020 <abdulsalam@live.in>
2023-12-03 18:49:44 +0530

    my first commit

---
+++ src/main/java/com/ab/learning/kuchbhi/KuchbhiApplication.java
@@ -0,0 +1,13 @@
+package com.ab.learning.kuchbhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuchbhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuchbhiApplication.class, args);
+	}
+
+}
